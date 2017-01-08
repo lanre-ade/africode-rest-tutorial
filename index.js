9b88d7fc7594e9ec23a9556910c90ba78d0a27d5
Lanre <adebambo@ualberta.ca>
2017-01-08 12:24:26 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var fs = require("fs");
+var app = express();
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/calculator.html');
+})
+
+app.get('/multiply', function (req, res) {
+  // TODO:: error check request parameters
+  var a  = parseFloat(req.query.a);
+  var b  = parseFloat(req.query.b);
+  var result = a * b;
+  res.end( JSON.stringify({
+    result: result,
+    message: "",
+    error: "",
+    status: 200
+  }));
+})
+
+app.get('/divide', function (req, res) {
+  // TODO:: error check request parameters
+  var a  = parseFloat(req.query.a);
+  var b  = parseFloat(req.query.b);
+  var result = a / b;
+  res.end( JSON.stringify({
+    result: result,
+    message: "",
+    error: "",
+    status: 200
+  }));
+})
+
+app.get('/add', function (req, res) {
+  // TODO:: error check request parameters
+  var a  = parseFloat(req.query.a);
+  var b  = parseFloat(req.query.b);
+  var result = a + b;
+  res.end( JSON.stringify({
+    result: result,
+    message: "",
+    error: "",
+    status: 200
+  }));
+})
+
+app.get('/subtract', function (req, res) {
+  // TODO:: error check request parameters
+  var a  = parseFloat(req.query.a);
+  var b  = parseFloat(req.query.b);
+  var result = a - b;
+  res.end( JSON.stringify({
+    result: result,
+    message: "",
+    error: "",
+    status: 200
+  }));
+})
+
+var _port = 8080
+var server = app.listen(_port, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})
